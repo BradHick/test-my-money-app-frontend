f872764519998f5bd75482f4ea18ed0b070a2daf
BradHick <bradleyhrm@gmail.com>
2018-07-18 12:35:26 -0300

    Create footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2018
+      <a href="https://www.linkedin.com/in/bradley-hick/" target='_blank'>Brad Hick</a>
+    </strong>
+  </footer>
+);
