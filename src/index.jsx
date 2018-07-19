50aece236b170b4718bb65a3cbc6d59cd35a0c10
BradHick <bradleyhrm@gmail.com>
2018-07-19 15:20:09 -0300

    Create first reducers

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,26 @@
+/**
+|--------------------------------------------------
+| Import modules
+|--------------------------------------------------
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+/**
+|--------------------------------------------------
+| Import components
+|--------------------------------------------------
+*/
 import App from './main/app';
+import reducers from './main/reducers';
 
+const store = createStore(reducers);
 ReactDOM.render(
-  <App />,
+  <Provider store={ store }>
+    <App />
+  </Provider>
+  ,
   document.getElementById('app')
 );
