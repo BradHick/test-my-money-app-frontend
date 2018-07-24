f09e4b7f500083698065872500838f91843cc89b
BradHick <bradleyhrm@gmail.com>
2018-07-24 10:12:55 -0300

    Adjust BillingCycleForm

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -8,7 +8,7 @@ class BillingCycleForm extends Component{
       <form role='form' onSubmit={ handleSubmit }>
         <div className="box-body">
           <Field name='name' component='input'/>
-          <Field name='nomnth' component='input'/>
+          <Field name='month' component='input'/>
           <Field name='year' component='input'/>
         </div>
         <div className="box-footer">
